488e9246c309eaec555510ef655be7701130c658
Tristan Pacba <tpacba@live.com>
2020-07-22 15:39:01 -0700

    generated prompts and confirms within function 'generatePassword()', returns values to see display

--- script.js
+++ script.js
@@ -2,6 +2,21 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+
+function generatePassword() {
+  var passLength = prompt("How long do your want your password to be?");
+  var smallCase = confirm("Do you want small cases?");
+  var upperCase = confirm("Do you want upper cases?");
+  var numCase = confirm("Do you want numbers?");
+  var specialCase = confirm("Do you want special cases?");
+
+
+  return "passLength:" + passLength + "; smallCase:" + smallCase + "; upperCase:" + upperCase + "; numCase:" + numCase + "; specialCase:" + specialCase;
+}
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -9,6 +24,5 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
