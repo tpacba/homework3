7a6d9402d39ce4e60e3298f242626a2b4aacf6b5
Tristan Pacba <tpacba@live.com>
2020-07-24 01:51:06 -0700

    final push until i frustratingly delete this one and come up with another solution, added the minus 1 for the function randomIntCount to apply real time counting

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function generatePassword() {
   do {
     // Prompt user for length
     var passLength = prompt("How long do your want your password to be? Must be at least 8 characters and no more than 128 characters.");
-    
+
     // Parse answer into an integer, empty or anything other than integers return NaN
     passLength = parseInt(passLength);
 
@@ -25,38 +25,42 @@ function generatePassword() {
       return "No password to give. Please try again.";
     }
   } while (passLength < 8 || passLength > 128) // Restrict the user from continuing with do while loop if prompt is less than 8 or greater than 128
-    
+
+  // Declare totalString outside of function to be unviversal function, will be empty for now to add characters later
   var totalString = "";
 
-  function addChar (type) {
+  // Create a function that adds characters into totalString
+  function addChar(type) {
     totalString = totalString + type;
     console.log("Characters included: " + totalString);
     return totalString;
   }
 
+  // Confirm the user whether or not they want to add specific characters, call the function addChar to add characters into totalString
   if (confirm("Do you want small cases?")) {
     addChar(alphabetSmall);
   }
-
   if (confirm("Do you want upper cases?")) {
     addChar(alphabetUpper);
   }
-
   if (confirm("Do you want numbers?")) {
     addChar(numberSign);
   }
-
   if (confirm("Do you want special characters?")) {
     addChar(specialSign);
   }
-  
+
+  // If user chose no on all confirms, return the try again message
   if (totalString == "") {
     return "No password to give. Please try again."
   } else {
+    // Declare empty passwordString outside of the for loop, will be added with random characters
     var passwordString = "";
+    // Run the for loop for whatever times the password length the user gave in the beginning
     for (var i = 0; i < passLength; i++) {
+      // Add characters from totalString at random into an empty passwordString then return it
       passwordString = passwordString + totalString.charAt(randomIntCount(0, totalString.length));
-    } 
+    }
     return passwordString;
   }
 }
